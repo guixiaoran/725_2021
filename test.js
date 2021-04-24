33ffbaab61fedfdf356885e34b5b6273b0ec84e3
guixiaoran <xgui@deakin.edu.au>
2021-04-25 01:33:38 +1000

    basic structure completed

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-console.log("hello, world")
+let express = require("express");
+let app = express();
+
+let http = require('http').createServer(app);
+
+var port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/public'));
+
+http.listen(port,()=>{
+    console.log("listening on port ",port);
+});
