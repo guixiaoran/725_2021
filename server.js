5b5e705984f8be5bd8d4afe24a67672fb8f5cfda
guixiaoran <xgui@deakin.edu.au>
2021-04-29 22:53:50 +1000

    all function finished

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ let express = require("express");
 let app = express();
 
 let http = require('http').createServer(app);
+let io =require('socket.io')(http);
 
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
+
+
+
+
+
 
 http.listen(port,()=>{
     console.log("listening on port ",port);
